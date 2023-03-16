88d8f67b71180b952096eee9f057e22f52e92c64
Earth Patel <eapa@odoo.com>
2023-03-16 19:09:32 +0530

    playlist completed

---
+++ music_player_eapa/models/player.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class music_player(models.Model):
+    _name = 'music_player.music_player'
+    _description = 'music_player.music_player'
+
+    name = fields.Char()
+    filename=fields.Char()
+    url=fields.Char(compute="_compute_url")
+
+    def _compute_url(self):
+        for record in self:
+            record.url=record.get_base_url() + '/music/'+str(record.id)

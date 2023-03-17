b72dcd0d5278d7547a0ec0f699a08d85db174ac5
Vraj Panchal <vrpa@odoo.com>
2023-03-17 11:28:14 +0530

    Day 3 Added play list solved the errors related too playlist

--- musicplayer/static/app.js
+++ musicplayer/static/app.js
@@ -6,13 +6,12 @@ class Player extends Component{
     static template = xml`
     <div >
     <h2 id = "song-title">Song Title</h2>
-    <div>
-    <button id ="pause-button" t-on-click="pauseThisSong">Pause</button>
-    <button id ="play-button" t-on-click="playThisSong">Play</button>
-    <button id ="stop-button" t-on-click="stopThisSong">Stop</button>
-    </div>
+      <div>
+        <button id ="pause-button" t-on-click="pauseThisSong">Pause</button>
+        <button id ="play-button" t-on-click="playThisSong">Play</button>
+        <button id ="stop-button" t-on-click="stopThisSong">Stop</button>
+      </div>
     </div>`;
-
     playThisSong() {
         if (!audio) {
           return;
@@ -38,7 +37,7 @@ class Player extends Component{
 class PlayList extends Component {
     static template = xml`
   <div id="PlayList" style="float: right">
-    <SongItems songitems="props.playlist"/>
+    
     <h2>Playlist</h2>
         <t t-if="props.playData[0]">     
             <t t-foreach="props.playData" t-as="song" t-key="song.id">
@@ -48,23 +47,32 @@ class PlayList extends Component {
             </t>
         </t>
   </div>
-    `;
-
+  `;
+  removeSongFromPlayList(ev) {
+    const selectedSongUrl = ev.target.getAttribute('value');
+    const selectedSong = this.props.playData.findIndex(song => song.url===selectedSongUrl);
+    this.props.playData.splice(selectedSong,1);
+  }
+  playSong(ev) {
+    const selectedSongUrl = ev.target.getAttribute('value');
+    const selectedSong = this.props.playData.find(song => song.url===selectedSongUrl);
+    document.getElementById("song-title").textContent = selectedSong.name;
+    audio = new Audio(selectedSongUrl);
+    audio.play();
+  }
     static props = ['playlist'];
- 
 }
 class MusicList extends Component{
     static template = xml`
     <div id ="MusicList" style="float:left">
-    <t t-if="props.searchData[0] and props.searchData[0] !== 'Song not Found'">
-    <h2>List of Songs</h2>
-    <t t-foreach="props.searchData[0]" t-as="song" t-key="song.id">
+      <t t-if="props.searchData[0] and props.searchData[0] !== 'Song not Found'">
+        <h2>List of Songs</h2>
+          <t t-foreach="props.searchData[0]" t-as="song" t-key="song.id">
             <p><t t-out="song.name"/></p>
-            <button t-att-value="song.url" t-on-click="addSongToPlaylist">Add to playlist</button>
             <button t-att-value="song.url" t-on-click="playSong">Play song</button>
-        </t>
-    </t>
-    </div>
+            <button t-att-value="song.url" t-on-click="addSongToPlaylist">Add to playlist</button>
+          </t>
+      </t>
     <Player/>
     </div>
     `;
@@ -94,19 +102,15 @@ class Search extends Component{
             <MusicList searchData="searchData" addToPlayList="props.addToPlayList" playData="props.playData"/>
         </div>
     `;
-
     setup(){
         this.searchData = useState([]);
-    
     }
     async getMusic(){
         const findSong = document.getElementById("searchSong").value;
         const response = await fetch(`/music/search?song_name=${findSong}`);
         const {result : newData} = await response.json();
         this.searchData.pop(newData)
-    
         this.searchData.push(newData)
-        
     }
     static components = {MusicList};
 }
@@ -125,7 +129,6 @@ class Root extends Component{
       this.playData.push(song)
     }
     static components = {Search,PlayList};
-
 }
 window.onload = function()
 {
